346786f994c7add927b3f40c1c099d08160b6ae4
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2024-11-19 17:32:36 +0100

    part 2 of 92 done without function if the text are saved

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,63 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private String fileName;
 
+    private static final String DEFOULT_FILE = "output.txt";
+    private static final String PATH = System.getProperty("user.home") + File.separator;
+
+    private Controller() {
+        this.fileName = DEFOULT_FILE;
+    }
+    
+    /**
+     * 
+     * @return The file name
+     */
+    public String getFileName() {
+        return this.fileName;
+    }
+    private void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    /**
+     * @param  fileName New file, that must be in the home directory, and it must be passed by only name,
+     *if the file doesn't exist the metod return False
+     * @return True if the file exist, False instead
+     */
+    public boolean setFile(String fileName) {
+        if(new File(PATH + fileName).exists()) {
+            setFileName(fileName);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    /**
+     * 
+     * @return The string of the file path
+     */
+    public String getFilePAth() {
+        return PATH + this.fileName;
+    }
+    /**
+     * 
+     * @param text The string to write in the file
+     */
+    public void write(String text) {
+        try (PrintStream ps = new PrintStream(this.getFilePAth(), StandardCharsets.UTF_8)){
+            ps.print(text);
+        } catch (IOException e1) {
+            System.out.println("The file is impossible to open or doesn't exist");// NOPMD: allowed as this is just an exercise
+            e1.printStackTrace(); // NOPMD: allowed as this is just an exercise
+        }
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.TextArea;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -10,4 +19,44 @@ public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
 
+    private static final int PROPORTION = 5;
+
+    /**
+     * Create a frame with the required specifications
+     */
+    public SimpleGUI() {
+        final JPanel canva = new JPanel();
+        final JButton save = new JButton("Save");
+        canva.setLayout(new BorderLayout());
+        canva.add(new TextArea(), BorderLayout.CENTER);
+        canva.add(save, BorderLayout.SOUTH);
+        /**
+         * Handlers
+         */
+        save.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                JOptionPane.showConfirmDialog(frame, "Are you sure to save?", "Saving", JOptionPane.YES_NO_OPTION);
+            }
+            
+        });
+        this.frame.setContentPane(canva);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    /** 
+     *Prepare the frame to be shown
+    */
+    public void display(){
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new SimpleGUI().display();
+    }
 }
