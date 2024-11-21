3b9d428e9f7c4e4c826760b5788bc2756c25a2c0
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2024-11-21 10:02:16 +0100

    implemented class ControllerImplementation

---
+++ 93-mvc-io/src/main/java/it/unibo/mvc/ControllerImplementation.java
@@ -0,0 +1,46 @@
+package it.unibo.mvc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ControllerImplementation implements Controller{
+    private List<String> chronology = new ArrayList<>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setString(String text) {
+        chronology.add(Objects.requireNonNull(text)); 
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getString() {
+        return chronology.getLast();
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getHistory() {
+        final List<String> copyOfChronology = new ArrayList<>();
+        copyOfChronology.addAll(this.chronology);
+        return copyOfChronology;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void stamp() {
+        final String  text = this.chronology.getLast();
+        if (text == null) {
+            throw new IllegalArgumentException("It's impossible to print a empty string");
+        } else {
+            System.out.println(text); // NOPMD: allowed as this is just an exercise
+        }
+    }
+
+}
