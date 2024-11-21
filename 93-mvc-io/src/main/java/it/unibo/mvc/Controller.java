8f3efff148536a931ab4924c6dfb6b3edcd513f2
Emanuele Fabricat <emanuele.fabricat@studio.unibo.it>
2024-11-21 09:45:48 +0100

    implemented interface Controller in 93

--- 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,31 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
-
+    /**
+     * A method for setting the next string to print. Null values are not
+     * acceptable, and an exception should be produced.
+     * 
+     * @param text The next string to print
+     */
+    public void setString(String text);
+    /**
+     * A method for getting the next string to print
+     * 
+     * @return The last String to print
+     */
+    public String getString();
+    /**
+     * A method for getting the history of the printed strings (in form of a {@link List#of(String)})
+     */
+    public List<String> getHistory();
+    /**
+     * A method that prints the current string. 
+     * If the current string is unset, an {@link IllegalStateException} should be thrown.
+     */
+    public void stamp();    
 }
